abfb49dd83ac71c656c40a716b8be86ba07ee7ba
Mario40000 <mario40000@hotmail.com>
2017-07-16 22:25:47 +0200

    Subida del proyecto

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class AICharacterControl : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public Transform[] WayPoints;                                    // target to aim for
+        private int siguienteWayPoint;
+        private int target = 0;
+
+
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+	        agent.updateRotation = false;
+	        agent.updatePosition = true;
+        }
+
+
+        private void Update()
+        {
+            if (WayPoints[target] != null)
+                agent.SetDestination(WayPoints[target].position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+                character.Move(agent.desiredVelocity, false, false);
+            else
+                character.Move(Vector3.zero, false, false);
+
+            if(agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+            {
+                target = (target + 1) % WayPoints.Length;
+            }
+
+            
+
+        }
+
+        
+
+        
+
+        
+    }
+}
